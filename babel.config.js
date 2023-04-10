3be83b93825a8c18783ee7257fda44afa9061b2c
bomstechnology <marcsigha@gmail.com>
2023-04-10 17:06:33 +0100

    maj

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    plugins: [
+      ["react-native-reanimated/plugin"], 
+      [
+        "module-resolver", 
+        {
+          "root": ["./src"],
+          "alias": {
+            "@screens" : "./src/screens",
+            "@components" : "./src/components",
+            "@store" : "./src/store",
+            "@data" : "./src/data",
+            "@navigators" : "./src/navigators",
+            "@constants" : "./src/constants",
+          }
+        }
+      ]
+    ]
   };
 };
