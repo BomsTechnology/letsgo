2beffd4b6a0a676e12fe19e9b25ef01b3a6198ab
Marcelin <marcsigha@gmail.com>
2023-07-04 15:04:32 +0100

    start drawer page

---
+++ src/screens/MoreOptionsScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MoreOptionsScreen = () => {
+  return (
+    <View>
+      <Text>MoreOptionsScreen</Text>
+    </View>
+  )
+}
+
+export default MoreOptionsScreen
+
+const styles = StyleSheet.create({})
